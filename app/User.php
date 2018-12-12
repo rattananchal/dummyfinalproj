bfee5a8f1b0aa9bbdb83e424c4f5d3d92811769d
anchal <akr43@njit.edu>
2018-12-12 13:58:56 -0500

    Task : Dummy users created

--- app/User.php
+++ app/User.php
@@ -41,7 +41,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        /*'name', */
+        'email', 'password',
     ];
 
     /**
